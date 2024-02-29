b9c6f06eedb3541d87aa7a63be33eb88b5eda717
yann-vulliet <70075639+yann-vulliet@users.noreply.github.com>
2024-02-29 13:57:04 +0100

    finish

--- App.jsx
+++ App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Search from './component/Search'
 import Banner from './assets/Banner.png'
+import Footer from './assets/footer.jpg'
 import { Image, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <Image style={styles.image} source={Banner} />
+        <Image style={styles.banner} source={Banner} />
         <View style={styles.search} >
           <Search />
         </View>
+        <Image style={styles.footer} source={Footer} />
       </View>
     </>
   );
@@ -20,10 +22,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  image: {
+  banner: {
     flex: 0,
     width: '100%',
     height: 170,
+  },
+  footer: {
+    flex: 0,
+    objectFit: 'cover',
+    width: '100%',
+    height: 70,
   },
   search: {
     flex: 4,
